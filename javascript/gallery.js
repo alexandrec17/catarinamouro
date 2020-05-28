01487a13ee8dfe4be50b3f16ce50dd83150bd7cf
Alexandre Carapeto <alexcarapeto@hotmail.com>
2020-05-28 03:11:59 +0100

    dynamic gallery

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -3,6 +3,7 @@ function removePortraitAndLandscapeStyles (elem) {
   elem.classList.remove('landscape')
   elem.classList.remove('portrait')
 }
+
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 function openModal (currentElem) {
   // Get the modal
@@ -13,46 +14,62 @@ function openModal (currentElem) {
   var modalImg = document.getElementById('modalImg')
   var captionText = document.getElementById('caption')
 
+  var prev = document.getElementById('prev')
+  var next = document.getElementById('next')
+  var span = document.getElementsByClassName('close')[0]
+
   removePortraitAndLandscapeStyles(modalImg)
+  removePortraitAndLandscapeStyles(next)
+  removePortraitAndLandscapeStyles(prev)
   captionText.classList.remove('landscape')
+
   if (currentElem.classList.contains('portrait')) {
     modalImg.classList.add('portrait')
+    prev.classList.add('portrait')
+    next.classList.add('portrait')
   } else {
     modalImg.classList.add('landscape')
     captionText.classList.add('landscape')
+    prev.classList.add('landscape')
+    next.classList.add('landscape')
   }
   modal.style.display = 'block'
 
   modalImg.src = img.src
   captionText.innerHTML = img.alt
 
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName('close')[0]
-
   // When the user clicks on <span> (x), close the modal
   span.onclick = function () {
     modal.style.display = 'none'
   }
+
+  prev.onclick = function () {
+    changeSlides(-1)
+  }
+  next.onclick = function () {
+    changeSlides(1)
+  }
 }
 
 var slideIndexJS = 1
-showSlides(slideIndexJS)
+// showSlides(slideIndexJS)
 
-function changeSlides (currentElem, n) {
-  showSlides(currentElem, slideIndexJS += n)
+function changeSlides (n) {
+  showSlides(slideIndexJS += n)
 }
 
-function currentSlide (currentElem, n) {
-  showSlides(currentElem, slideIndexJS = n)
+function currentSlide (n) {
+  showSlides(slideIndexJS = n)
 }
 
-function showSlides (currentElem, n) {
-  /* var i
-  var slidesJS = document.getElementsByClassName('my-slides')
+function showSlides (n) {
+  var slidesJS = document.getElementsByClassName('gallery-image')
+
   if (n > slidesJS.length) { slideIndexJS = 1 }
   if (n < 1) { slideIndexJS = slidesJS.length }
-  for (i = 0; i < slidesJS.length; i++) {
-    $(slidesJS[i]).css('display', 'none')
-  }
-  $(slidesJS[slideIndexJS - 1]).css('display', 'block') */
+
+  var modal = document.getElementById('myModal')
+  modal.style.display = 'none'
+
+  openModal(slidesJS[slideIndexJS - 1])
 }
